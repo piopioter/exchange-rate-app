7b4a6d814fbccd301f9cbde2e900b0f4a1e87d6e
Piotr Czernik <czernik.pio@gmail.com>
2023-08-04 19:04:00 +0200

    initial commit

---
+++ src/main/java/pl/kurs/exchangerateapponspringbootgit/ExchangeRateAppOnSpringbootGitApplication.java
@@ -0,0 +1,13 @@
+package pl.kurs.exchangerateapponspringbootgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateAppOnSpringbootGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRateAppOnSpringbootGitApplication.class, args);
+    }
+
+}
